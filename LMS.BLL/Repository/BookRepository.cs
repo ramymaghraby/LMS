bab5318de502a599ba7538542c47f2ea6ed01210
ramymaghraby <ramymaghraby@gmail.com>
2025-10-29 23:43:56 +0300

    book custom repo

--- LMS.BLL/Repository/BookRepository.cs
+++ LMS.BLL/Repository/BookRepository.cs
@@ -1,4 +1,6 @@
-﻿using LMS.DAL.Entities;
+﻿using LMS.DAL.Database;
+using LMS.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,49 @@ namespace LMS.BLL.Repository
 {
     public class BookRepository : GenericRepository<Book>
     {
+        ApplicationContext _db = new ApplicationContext();
+        public async Task<Book> GetHighestRating()
+        {
+            var result = await _db.Set<Book>().OrderByDescending(book => book.Rate).FirstOrDefaultAsync();
+            return result;
+        }
+        public async Task<Book> GetLowestRating()
+        {
+            var result = await _db.Set<Book>().OrderBy(book => book.Rate).FirstOrDefaultAsync();
+            return result;
+        }
+        public async Task<(Book Book, int BorrowCount)> GetMostBorrowed()
+        {
+            var result = await _db.BorrowTransactions
+                .GroupBy(b => b.BookId)
+                .Select(g => new
+                {
+                    BookId = g.Key,
+                    BorrowCount = g.Count()
+                })
+                .OrderByDescending(x => x.BorrowCount)
+                .FirstOrDefaultAsync();
+            if (result == null)
+                return (null, 0);
+            var book = await _db.Books.FirstOrDefaultAsync(b => b.Id == result.BookId);
+            return (book, result.BorrowCount);
+        }
+        public async Task<(Book Book, int BorrowCount)> GetLeastBorrowed()
+        {
+            var result = await _db.BorrowTransactions
+                .GroupBy(b => b.BookId)
+                .Select(g => new
+                {
+                    BookId = g.Key,
+                    BorrowCount = g.Count()
+                })
+                .OrderBy(x => x.BorrowCount)
+                .FirstOrDefaultAsync();
+            if (result == null)
+                return (null, 0);
+            var book = await _db.Books.FirstOrDefaultAsync(b => b.Id == result.BookId);
+            return (book, result.BorrowCount);
+        }
 
     }
 }
