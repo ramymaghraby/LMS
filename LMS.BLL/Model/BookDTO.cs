d23aaece42873609856001c327545ba463c1f6db
ramymaghraby <ramymaghraby@gmail.com>
2025-11-01 01:56:46 +0200

    added BookDTO

---
+++ LMS.BLL/Model/BookDTO.cs
@@ -0,0 +1,30 @@
+﻿using LMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Model
+{
+    public class BookDTO
+    {
+        public BookDTO()
+        {
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int PublishedYear { get; set; }
+        public int Rate { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
