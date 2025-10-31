820d6bdac3054e54952ce017318455fbb00cdab0
ramymaghraby <ramymaghraby@gmail.com>
2025-10-31 19:43:15 +0200

    custom UserRepository

---
+++ LMS.BLL/Repository/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using LMS.DAL.Database;
+using LMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        ApplicationContext _db = new ApplicationContext();
+        public async Task<bool> DeReActivateUser(int memberId)
+        {
+            var member = await _db.Members.FindAsync(memberId);
+            if (member == null)
+                return false;
+            member.IsActive = !member.IsActive;
+            await _db.SaveChangesAsync();
+            return true;
+        }
+    }
+    public interface IUserRepository
+    {
+        Task<bool> DeReActivateUser(int memberId);
+    }
+}
+
