ea8772b3e6ea3eaca2d3b359350d334952bd9ed9
ramymaghraby <ramymaghraby@gmail.com>
2025-11-01 14:25:58 +0200

    Role Service and DTO

---
+++ LMS.BLL/Model/RoleDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Model
+{
+    public class RoleDTO
+    {
+        public RoleDTO() {
+            IsActive = true;
+            CreatedOn = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ LMS.BLL/Service/RoleService.cs
@@ -0,0 +1,99 @@
+﻿using LMS.BLL.Model;
+using LMS.BLL.Repository;
+using LMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Service
+{
+    public class RoleService : IRoleService
+    {
+        GenericRepository<Role> _roleRepo = new GenericRepository<Role>();
+
+        public async Task<List<RoleDTO>> GetAllRoles()
+        {
+            var roles = await _roleRepo.GetAll(r => r.IsActive);
+            var rolesDTO = new List<RoleDTO>();
+            if (roles == null) 
+                return rolesDTO;
+            foreach (var role in roles) {
+                var roleDTO = new RoleDTO()
+                {
+                    Name = role.Name,
+                    Id = role.Id,
+                    IsActive = role.IsActive,
+                    CreatedOn = role.CreatedOn,
+                    Description = role.Description
+                    
+                };
+                rolesDTO.Add(roleDTO);
+            };
+            return rolesDTO;
+        }
+
+        public async Task<RoleDTO> GetRoleById(int roleId)
+        {
+            var role = await _roleRepo.GetBy(c => c.Id == roleId);
+            var roleDTO = new RoleDTO()
+            {
+                Name = role.Name,
+                Id = role.Id,
+                IsActive = role.IsActive,
+                CreatedOn = role.CreatedOn,
+                Description = role.Description
+                
+
+            };
+            return roleDTO;
+
+        }
+        public async Task CreateRole(RoleDTO roleDTO)
+        {
+            var role = new Role() 
+            {
+                Name = roleDTO.Name,
+                Id = roleDTO.Id,
+                IsActive = roleDTO.IsActive,
+                CreatedOn = roleDTO.CreatedOn,
+                Description = roleDTO.Description
+            };
+            await _roleRepo.CreateAsync(role);
+        }
+        public async Task UpdateRole(RoleDTO roleDTO)
+        {
+            var role = new Role()
+            {
+                Name = roleDTO.Name,
+                Id = roleDTO.Id,
+                IsActive = roleDTO.IsActive,
+                CreatedOn = roleDTO.CreatedOn,
+                Description = roleDTO.Description
+            };
+            await _roleRepo.UpdateAsync(role);
+        }
+        public async Task DeleteRole(RoleDTO roleDTO)
+        {
+            var role = new Role()
+            {
+                Name = roleDTO.Name,
+                Id = roleDTO.Id,
+                IsActive = roleDTO.IsActive,
+                CreatedOn = roleDTO.CreatedOn,
+                Description = roleDTO.Description,
+            };
+            await _roleRepo.DeleteAsync(role);
+
+        }
+    }
+    public interface IRoleService
+    {
+        Task<List<RoleDTO>> GetAllRoles();
+        Task<RoleDTO> GetRoleById(int RoleId);
+        Task CreateRole(RoleDTO roleDTO);
+        Task UpdateRole(RoleDTO roleDTO);
+        Task DeleteRole(RoleDTO roleDTO);
+    }
+}
