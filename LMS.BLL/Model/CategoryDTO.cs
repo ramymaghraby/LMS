90b045884c63e174d6ef377e67c4fbf2eef6b172
ramymaghraby <ramymaghraby@gmail.com>
2025-11-01 14:25:31 +0200

    Category Service and DTO

---
+++ LMS.BLL/Model/CategoryDTO.cs
@@ -0,0 +1,25 @@
+﻿using LMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Model
+{
+    public class CategoryDTO
+    {
+        public CategoryDTO()
+        {
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
---
+++ LMS.BLL/Service/CategoryService.cs
@@ -0,0 +1,102 @@
+﻿using LMS.BLL.Model;
+using LMS.BLL.Repository;
+using LMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Service
+{
+    public class CategoryService : ICategoryService
+    {
+        GenericRepository<Category> _categoryRepo = new GenericRepository<Category>();
+
+        public async Task<List<CategoryDTO>> GetAllCategroies()
+        {
+            var categories = await _categoryRepo.GetAll(c => c.IsActive);
+            var categoriesDTO = new List<CategoryDTO>();
+            if (categories == null) 
+                return categoriesDTO;
+            foreach (var category in categories) {
+                var categoryDTO = new CategoryDTO()
+                {
+                    Name = category.Name,
+                    Id = category.Id,
+                    IsActive = category.IsActive,
+                    CreatedOn = category.CreatedOn,
+                    Description = category.Description,
+                    Books = category.Books
+                };
+                categoriesDTO.Add(categoryDTO);
+            };
+            return categoriesDTO;
+        }
+
+        public async Task<CategoryDTO> GetCategoryById(int categoryId)
+        {
+            var category = await _categoryRepo.GetBy(c => c.Id == categoryId);
+            var categoryDTO = new CategoryDTO()
+            {
+                Name = category.Name,
+                Id = category.Id,
+                IsActive = category.IsActive,
+                CreatedOn = category.CreatedOn,
+                Description = category.Description,
+                Books = category.Books
+
+            };
+            return categoryDTO;
+
+        }
+        public async Task CreateCategory(CategoryDTO categoryDTO)
+        {
+            Category category = new Category() 
+            {
+                Name = categoryDTO.Name,
+                Id = categoryDTO.Id,
+                IsActive = categoryDTO.IsActive,
+                Books = categoryDTO.Books,
+                CreatedOn = categoryDTO.CreatedOn,
+                Description = categoryDTO.Description,
+            };
+            await _categoryRepo.CreateAsync(category);
+        }
+        public async Task UpdateCategory(CategoryDTO categoryDTO)
+        {
+            Category category = new Category()
+            {
+                Name = categoryDTO.Name,
+                Id = categoryDTO.Id,
+                IsActive = categoryDTO.IsActive,
+                Books = categoryDTO.Books,
+                CreatedOn = categoryDTO.CreatedOn,
+                Description = categoryDTO.Description,
+            };
+            await _categoryRepo.UpdateAsync(category);
+        }
+        public async Task DeleteCategory(CategoryDTO categoryDTO)
+        {
+            Category category = new Category()
+            {
+                Name = categoryDTO.Name,
+                Id = categoryDTO.Id,
+                IsActive = categoryDTO.IsActive,
+                Books = categoryDTO.Books,
+                CreatedOn = categoryDTO.CreatedOn,
+                Description = categoryDTO.Description,
+            };
+            await _categoryRepo.DeleteAsync(category);
+
+        }
+    }
+    public interface ICategoryService
+    {
+        Task<List<CategoryDTO>> GetAllCategroies();
+        Task<CategoryDTO> GetCategoryById(int categoryId);
+        Task CreateCategory(CategoryDTO categoryDTO);
+        Task UpdateCategory(CategoryDTO categoryDTO);
+        Task DeleteCategory(CategoryDTO categoryDTO);
+    }
+}
