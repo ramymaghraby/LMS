de17e124b0eea22ab7f888208288974dd6546f8f
ramymaghraby <ramymaghraby@gmail.com>
2025-11-01 15:14:10 +0200

    User Service and DTO

---
+++ LMS.BLL/Model/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Model
+{
+    public class UserDTO
+    {
+        public UserDTO()
+        {
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ LMS.BLL/Service/UserService.cs
@@ -0,0 +1,116 @@
+﻿using LMS.BLL.Model;
+using LMS.BLL.Repository;
+using LMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.BLL.Service
+{
+    public class UserService : IUserService
+    {
+        UserRepository _userRepo = new UserRepository();
+        public async Task<List<UserDTO>> GetAllUsers()
+        {
+            var users = await _userRepo.GetAll(u => u.IsActive);
+            var usersDTO = new List<UserDTO>();
+            if (users == null)
+                return usersDTO;
+            foreach (var user in users)
+            {
+                var userDTO = new UserDTO()
+                {
+                    IsActive = user.IsActive,
+                    CreatedOn = user.CreatedOn,
+                    Email = user.Email,
+                    Id = user.Id,
+                    Password = user.Password,
+                    Username = user.Username
+                };
+                usersDTO.Add(userDTO);
+            }
+            ;
+            return usersDTO;
+        }
+
+        public async Task<UserDTO> GetUserById(int userId)
+        {
+            var user = await _userRepo.GetBy(u => u.Id == userId);
+            
+                var userDTO = new UserDTO()
+                {
+                    IsActive = user.IsActive,
+                    CreatedOn = user.CreatedOn,
+                    Email = user.Email,
+                    Id = user.Id,
+                    Password = user.Password,
+                    Username = user.Username
+                };
+            
+            
+            
+            return userDTO;
+        }
+        public async Task CreateUser(UserDTO userDTO)
+        {
+            var user = new User()
+            {
+                Id = userDTO.Id,
+                IsActive = userDTO.IsActive,
+                CreatedOn = userDTO.CreatedOn,
+                Email = userDTO.Email,
+                Username= userDTO.Username,
+                Password = userDTO.Password
+                
+            };
+            await _userRepo.CreateAsync(user);
+        }
+        public async Task UpdateUser(UserDTO userDTO)
+        {
+            var user = new User()
+            {
+                Id = userDTO.Id,
+                IsActive = userDTO.IsActive,
+                CreatedOn = userDTO.CreatedOn,
+                Email = userDTO.Email,
+                Username = userDTO.Username,
+                Password = userDTO.Password
+
+            };
+            await _userRepo.UpdateAsync(user);
+        }
+        public async Task DeleteUser(UserDTO userDTO)
+        {
+            var user = new User()
+            {
+                Id = userDTO.Id,
+                IsActive = userDTO.IsActive,
+                CreatedOn = userDTO.CreatedOn,
+                Email = userDTO.Email,
+                Username = userDTO.Username,
+                Password = userDTO.Password
+
+            };
+            await _userRepo.DeleteAsync(user);
+        }
+
+        public async Task<bool> DeReActivateUser(int userId)
+        {
+            await _userRepo.DeReActivateUser(userId);
+            return true;
+        }
+
+        
+    }
+    public interface IUserService
+    {
+        Task<List<UserDTO>> GetAllUsers();
+        Task<UserDTO> GetUserById(int userId);
+        Task<bool> DeReActivateUser(int userId);
+        Task CreateUser(UserDTO userDTO);
+        Task UpdateUser(UserDTO userDTO);
+        Task DeleteUser(UserDTO userDTO);
+    }
+}
